f9eb2cf18b5c9dc177b3a2d0272c1003a3bae328
LinkachuGaming <LinkachuGaming@gmail.com>
2024-11-23 23:37:21 -0500

    Stackable Neoforge Fixes some existing bugs that broke support

--- src/main/java/com/lonkachu/mixin/DataComponentTypesFixin.java
+++ src/main/java/com/lonkachu/mixin/DataComponentTypesFixin.java
@@ -2,6 +2,7 @@ package com.lonkachu.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.lonkachu.stackable.Stackable;
+import com.mojang.serialization.Codec;
 import net.minecraft.core.component.DataComponents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,5 +19,14 @@ public class DataComponentTypesFixin {
     private static int getMaxCountPerStack(int original) {
         //return 128;
         return Stackable.getMaxStackCount();
+    }
+    @ModifyExpressionValue
+            (
+                    method = "lambda$static$1",
+                    at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ExtraCodecs;intRange(II)Lcom/mojang/serialization/Codec;")
+            )
+    private static Codec<Integer> replaceCodec(Codec<Integer> original)
+    {
+        return Codec.intRange(0, Stackable.getMaxStackCount());
     }
 }
